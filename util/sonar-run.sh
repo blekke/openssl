c8aac85f78ceeaebdb6cf9bc1f8583769612f07d
Davide Galassi <davxy@datawork.net>
2019-08-24 20:24:10 +0200

    Added Vera++ checks

--- util/sonar-run.sh
+++ util/sonar-run.sh
@@ -11,8 +11,13 @@ rm -rf sonar-data
 mkdir sonar-data
 
 # CPPcheck: a general purpose static code checker
-echo "Running cppcheck on $SRC..."
-
+echo "Running cppcheck on $SRC ..."
 cppcheck -Dinline=inline --language=c --force --enable=all --suppress=variableScope --suppress=unusedFunction --suppress=missingIncludeSystem --xml $INC $SRC 2> sonar-data/cppcheck-report.xml
 
-cppcheck-htmlreport --title=OpenSSL --file=sonar-data/cppcheck-report.xml --report-dir=sonar-data/html
+# Vera++: static code checker focusing on style issues
+VERA_SKIP="T0(10|11|12|13|19)"
+echo "Running vera++ on $SRC ..."
+find $SRC include -type f -regextype sed -iregex ".*/*\.\(c\|h\)" -print | vera++ - -showrules -nodup 2> vera.tmp
+cat vera.tmp | grep -v -E $VERA_SKIP | util/vera2report.perl > sonar-data/vera-report.xml
+rm vera.tmp
+
